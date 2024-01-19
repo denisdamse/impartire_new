cab7a5075405c4435dc6eaa80c74934a02bbd93f
denisdamse <denisdamse@gmail.com>
2024-01-19 10:50:52 +0200

    final program

--- Program.cs
+++ Program.cs
@@ -3,7 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Deimpartitul: ");
+        int deimp = int.Parse(Console.ReadLine());
+        Console.WriteLine("Impartitorul: ");
+        int imp = int.Parse(Console.ReadLine());
+
+        if (deimp % imp == 0)
+        {
+            Console.WriteLine("Catul este " + deimp / imp);
+        }
+        else
+            do
+            {
+                Console.WriteLine("Cele 2 nu se pot imparti exact, datile din nou");
+                Console.WriteLine("Deimpartitul: ");
+                deimp = int.Parse(Console.ReadLine());
+                Console.WriteLine("Impartitorul: ");
+                imp = int.Parse(Console.ReadLine());
+            } while (deimp % imp != 0);
+
     }
 }
-
